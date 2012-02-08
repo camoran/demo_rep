b8c45e233ebdfc7c6410161ef4fa59186eea8304
camoran <Duck.is.darkwarrior@gmail.com>
2012-02-08 22:24:10 +0200

    first

---
+++ first/src/java/controller/Name.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+/**
+ *
+ * @author Raymond
+ */
+public class Name {
+    private String value;  
+      
+    public String getValue() {
+        return value;
+    }
+    
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
---
+++ first/src/java/service/HelloService.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+/**
+ *
+ * @author Raymond
+ */
+public class HelloService {
+    public String sayHello(String name) {
+        return "Hello " + name + "!";        
+    }    
+}
